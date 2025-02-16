247de36f26b59d68592796d7e8f0e6dcd617062f
gabriel torres <gtghosth007@gmail.com>
2025-02-16 14:44:23 -0300

    implementando testes do nestJS

--- Backend/src/form/form.controller.spec.ts
+++ Backend/src/form/form.controller.spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FormController } from './form.controller';
+import { FormService } from './form.service';
+import { HttpModule } from '@nestjs/axios';  
 
 describe('FormController', () => {
   let controller: FormController;
+  let service: FormService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule], 
       controllers: [FormController],
+      providers: [FormService],
     }).compile();
 
     controller = module.get<FormController>(FormController);
+    service = module.get<FormService>(FormService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
 });
--- Backend/src/form/form.service.spec.ts
+++ Backend/src/form/form.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FormService } from './form.service';
+import { HttpModule } from '@nestjs/axios';
 
 describe('FormService', () => {
   let service: FormService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule], 
       providers: [FormService],
     }).compile();
 
